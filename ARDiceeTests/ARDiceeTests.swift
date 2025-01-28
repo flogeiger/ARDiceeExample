99562e8d344a9fc1ae526a1b6fbfd162e5cb13cb
fgeiger <florian.geiger21@gmail.com>
2025-01-28 17:56:34 +0100

    Initial Commit

---
+++ ARDiceeTests/ARDiceeTests.swift
@@ -0,0 +1,17 @@
+//
+//  ARDiceeTests.swift
+//  ARDiceeTests
+//
+//  Created by Florian Geiger on 27.01.25.
+//
+
+import Testing
+@testable import ARDicee
+
+struct ARDiceeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
